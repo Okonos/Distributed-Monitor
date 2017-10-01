23ab80f06cb16dfc2e3ae125f40bafee791c03cc
Okonos <tepyzboj@gmail.com>
2017-10-01 12:54:38 +0200

    Clean up main.go

--- main.go
+++ main.go
@@ -1,32 +1,6 @@
 package main
 
-import (
-	"log"
-	"time"
-)
-
 func main() {
-	log.SetFlags(log.Lshortfile)
 	s := newServer()
-	bcastAt := time.Now().Add(2 * time.Second)
-
 	s.loop()
-
-	for {
-		now := time.Now()
-		if now.After(bcastAt) {
-			// iface.Send("BROADCAST", "czesc")
-			bcastAt = now.Add(2 * time.Second)
-		}
-
-		// msg, err := iface.Recv() // non-blocking
-		// if err != nil {
-		// 	if err == zmq.AsErrno(syscall.EAGAIN) {
-		// 		continue
-		// 	} else {
-		// 		log.Fatalln(err)
-		// 	}
-		// }
-		// fmt.Println(strings.Join(msg, " "))
-	}
 }
